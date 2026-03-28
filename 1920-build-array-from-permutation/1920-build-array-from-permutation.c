c6f4ba3cc73b1a92e6149ea7021ec7251dabd29b
Muthukaruppan S <muthukaruppan18k@gmail.com>
2026-03-28 11:35:49 +0530

    Time: 0 ms (100%), Space: 15.3 MB (30.24%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.c
@@ -0,0 +1,13 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* buildArray(int* nums, int numsSize, int* returnSize) {
+    int* res = malloc(numsSize * sizeof(int));
+    for (int i=0;i < numsSize;i++ ){
+        res[i]=nums[nums[i]];
+    }
+    *returnSize = numsSize;
+    return res;
+
+
+}
