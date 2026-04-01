765e116ff07767233d20f516c149045586ac98e7
Muthukaruppan S <muthukaruppan18k@gmail.com>
2026-04-01 08:43:43 +0530

    Time: 1 ms (100%), Space: 42.7 MB (52.04%) - LeetHub

---
+++ 3270-find-the-key-of-the-numbers/3270-find-the-key-of-the-numbers.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int generateKey(int num1, int num2, int num3) {
+        int ans = 0, x = 1;
+        while(num1 > 0 || num2 > 0 || num3 > 0) {
+            int min_digit = Math.min((num1 % 10) , (num2 % 10));
+            min_digit = Math.min((num3 % 10) , min_digit);
+            ans += x * min_digit;
+            x *= 10;
+            num1 /= 10;
+            num2 /= 10;
+            num3 /= 10;
+        }
+        return ans;
+    }
+}
